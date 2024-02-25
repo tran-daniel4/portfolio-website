a55a159cec2928919773cacd800b3c2051cef6ff
bigwhippy <69910521+bigwhippy@users.noreply.github.com>
2024-02-25 15:23:33 -0500

    Navbar setup

---
+++ website/app/components/Navbar.js
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import box from '../../public/box.svg';
+import asset1 from '../../public/asset (1).svg';
+import asset2 from '../../public/asset (2).svg';
+
+export default function Navbar() {
+  return (
+    <div>
+
+      <header className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center py-5">
+          <div className="flex flex-column">
+          <span className="relative block sm:inline-block mr-2 w-3 h-2 ">
+            <Image src={box} alt="Box"  />
+            <Image src={asset1} alt="Box" />
+            <Image src={asset2} alt="Box" />
+          </span>
+          <h2 className="text-xl font-medium">dntr</h2>
+
+          </div>
+          <nav className="flex gap-16 font-medium font-jost">
+            <a href="Home" className="text-xl hover:text-gray-300">Home</a>
+            <a href="Resume" className="text-xl hover:text-gray-300">Resume</a>
+            <a href="Projects" className="text-xl hover:text-gray-300">Projects</a>
+            <a href="Contact" className="text-xl hover:text-gray-300">Contact</a>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
---
+++ website/app/pages/Contact.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <main>
+            <h1>Contact</h1>
+        </main>
+    );
+  }
+  
