3acd99ef4d4e13987b407949f3e64af57490401a
bigwhippy <69910521+bigwhippy@users.noreply.github.com>
2024-02-25 15:56:29 -0500

    finished navbar styling

--- website/app/components/Navbar.js
+++ website/app/components/Navbar.js
@@ -9,14 +9,15 @@ export default function Navbar() {
 
       <header className="bg-gray-800 text-white p-4">
         <div className="container mx-auto flex justify-between items-center py-5">
-          <div className="flex flex-column">
-          <span className="relative block sm:inline-block mr-2 w-3 h-2 ">
-            <Image src={box} alt="Box"  />
-            <Image src={asset1} alt="Box" />
-            <Image src={asset2} alt="Box" />
-          </span>
-          <h2 className="text-xl font-medium">dntr</h2>
-
+          <div className="flex items-center ">
+            <span className="mr-5 w-3 h-2">
+              <div className="flex flex-row items-center justify-center space-x-1">
+              <Image src={box} alt="Box"  />
+              <Image src={asset1} alt="Box" />
+              <Image src={asset2} alt="Box" />
+              </div>
+            </span>
+            <h2 className="text-xl font-medium">dntr</h2>
           </div>
           <nav className="flex gap-16 font-medium font-jost">
             <a href="Home" className="text-xl hover:text-gray-300">Home</a>
